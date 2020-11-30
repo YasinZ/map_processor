016c80db930d64cfa5f0a11c50796af6aa31b2c3
Yasin <kzuhurud@uci.edu>
2020-11-30 13:22:48 -0800

    New code for showing robot position

--- src/map_processor.cpp
+++ src/map_processor.cpp
@@ -1,70 +1,83 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "nav_msgs/OccupancyGrid.h"
 #include "tf/tfMessage.h"
-#include "geometry_msgs/PoseStamped.h"
-#include "geometry_msgs/Polygon.h"
-#include "map_processor/process.h"
-#include "map_processor/fetch.h"
-#include <tf/transform_datatypes.h>
-#include "map_processor/path.h"
 #include <sstream>
-#include <queue>
 #include <fstream>
 #include <cmath>
+#include "map_processor/save.h"
+#include "map_processor.h"
 
-// AMCL
-nav_msgs::OccupancyGrid::ConstPtr _grid;
-geometry_msgs::PoseStamped::ConstPtr _ps;
-bool showPath = false;
-
-struct Checkpoint { std::string name; geometry_msgs::PoseStamped::ConstPtr ps; };
-struct Point { int x, y; };
-struct Node { Point pt; int dist; std::vector<Point> path; };
-std::vector<Checkpoint> checkpoints;
-Node last;
-
-typedef int16_t coord_t;
-
-tf::Transform mapToWorld (const nav_msgs::MapMetaData& info)
+bool mapSaveCallback(map_processor::save::Request& req,
+                     map_processor::save::Response& resp)
 {
-  tf::Transform world_to_map;
-  tf::poseMsgToTF (info.origin, world_to_map);
-  return world_to_map;
+  save();
+  return true;
 }
 
-tf::Transform worldToMap (const nav_msgs::MapMetaData& info)
-{
-  return mapToWorld(info).inverse();
-}
- 
-struct Cell
-{
-   Cell(const coord_t x=0, const coord_t y=0): x(x), y(y) {}
-   coord_t x;
-   coord_t y;
- 
-   bool operator== (const Cell& c) const;
-   bool operator< (const Cell& c) const;
-};
+void save() {
+  std::string mapdatafile = "map.ppm";
+  ROS_INFO("Writing map occupancy data to %s", mapdatafile.c_str());
+  FILE* out = fopen(mapdatafile.c_str(), "w");
+  if (!out)
+  {
+    ROS_ERROR("Couldn't save map file to %s", mapdatafile.c_str());
+    return;
+  }
 
-bool IsValid(int row, int col) {
-  return (row >= 0) && (row < _grid->info.width) &&
-         (col >= 0) && (col < _grid->info.height);
-}
+  fprintf(out, "P6\n# CREATOR: map_saver.cpp %.3f m/pix\n%d %d\n255\n",
+          _grid->info.resolution, _grid->info.width, _grid->info.height);
+  for(unsigned int y = 0; y < _grid->info.height; y++) {
+    for(unsigned int x = 0; x < _grid->info.width; x++) {
+      unsigned int i = x + (_grid->info.height - y - 1) * _grid->info.width;
+      if (_grid->data[i] >= 0 && _grid->data[i] <= 0) { // [0,free)
+        Cell position = pointCell(_grid->info, _ps->pose.position);
+        if(position.x == x && position.y == _grid->info.height -y - 1) {
+          fputc(0, out);
+          fputc(0, out);
+          fputc(255, out);
+          continue;
+        } else {
+          fputc(254, out);
+          fputc(254, out);
+          fputc(254, out);
+        }
 
-int rowNum[] = {-1, 0, 0, 1};
-int colNum[] = {0, -1, 1, 0};
+        // if(showPath) {
+        //   bool found = false;
+        //   for(Point pt: last.path) {
+        //     if(pt.x == x && pt.y == _grid->info.height - y - 1) {
+        //       fputc(111, out);
+        //       fputc(222, out);
+        //       fputc(055, out);
+        //       found = true;
+        //       break;
+        //     }
+        //   }
 
-Node BFS(Point src, Point dest);
+        //   if(!found) {
+        //     fputc(254, out);
+        //     fputc(254, out);
+        //     fputc(254, out);
+        //   }
+        // } else {
+        //   fputc(111, out);
+        //   fputc(111, out);
+        //   fputc(111, out);
+        // }
+      } else if (_grid->data[i] >= 100) { // (occ,255]
+        fputc(000, out);
+        fputc(000, out);
+        fputc(000, out);
+      } else { //occ [0.25,0.65]
+        fputc(205, out);
+        fputc(205, out);
+        fputc(205, out);
+      }
+    }
+  }
 
-Cell pointCell (const nav_msgs::MapMetaData& info, const geometry_msgs::Point& p)
-{
-   tf::Point pt;
-   tf::pointMsgToTF(p, pt);
-   tf::Point p2 = worldToMap(info)*pt;
-   return Cell(floor(p2.x()/info.resolution), floor(p2.y()/info.resolution));
+  fclose(out);
 }
 
 void slam_out_poseCallback(const geometry_msgs::PoseStamped::ConstPtr& ps) {
@@ -115,80 +128,9 @@ void slam_out_poseCallback(const geometry_msgs::PoseStamped::ConstPtr& ps) {
   }
   
   std::ofstream o("src/map_processor/src/map.txt");
+  // save();
   o << oss.str().c_str();
   o.close();
-  // ROS_INFO("MAP:\n%s", oss.str().c_str());
-}
-
-Node BFS(Point src, Point dest) {
-  // ASSUME DESTINATION AND SOURCE ARE VALID
-  bool **visited = new bool *[_grid->info.height];
-  for(int i = 0; i < _grid->info.height; i++) {
-    visited[i] = new bool[_grid->info.height];
-    for(int j = 0; j < _grid->info.height; j++)
-      visited[i][j] = false;
-  }
-
-  visited[src.x][src.y] = true;
-  std::queue<Node> q;
-
-  Node s = {src, 0};
-  q.push(s);
-
-  while (!q.empty()) {
-    Node current = q.front();
-    Point point = current.pt;
-
-    if(point.x == dest.x && point.y == dest.y) {
-      return current;
-    }
-    
-    q.pop();
-
-    for(int i = 0; i < 4; i++) {
-      int row = point.x + rowNum[i];
-      int col = point.y + colNum[i];
-
-      if(IsValid(row, col) && _grid->data[row * _grid->info.height + col] == 0 && !visited[row][col]) 
-      {
-        visited[row][col] = true;
-        std::vector<Point> newPath = current.path;
-        newPath.push_back({row, col});
-        Node adj = { {row, col}, current.dist + 1, newPath};
-        q.push(adj);
-      }
-    }
-  }
-
-  return Node{ {-1, -1}, -1};
-}
-
-void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& grid)
-{
-  _grid = grid;
-}
-
-bool mapCheckpointCallback(map_processor::process::Request& req, 
-                           map_processor::process::Response& resp) {
-  checkpoints.push_back( Checkpoint{req.msg, _ps} );
-
-  for(auto checkpoint: checkpoints) {
-    // ROS_INFO("Checkpoint name: %s", checkpoint.name.c_str());
-    resp.results.push_back(checkpoint.name);
-  }
-
-  return true;
-}
-
-bool mapFetchCallback(map_processor::fetch::Request& req, 
-                      map_processor::fetch::Response& resp) {
-  for(auto checkpoint: checkpoints) {
-    resp.results.push_back(checkpoint.name);
-    resp.xs.push_back(checkpoint.ps->pose.position.x);
-    resp.ys.push_back(checkpoint.ps->pose.position.y);
-  }
-
-  return true;
 }
 
 bool mapPathCallback(map_processor::path::Request& req,
@@ -219,7 +161,6 @@ bool mapPathCallback(map_processor::path::Request& req,
   return true;
 }
 
-
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "map_processor");
@@ -230,6 +171,7 @@ int main(int argc, char **argv)
   ros::ServiceServer checkpoint_server = n.advertiseService("map_checkpoint", mapCheckpointCallback);
   ros::ServiceServer fetch_server = n.advertiseService("map_fetch", mapFetchCallback);
   ros::ServiceServer path_server = n.advertiseService("map_path", mapPathCallback);
+  ros::ServiceServer save_server = n.advertiseService("map_save", mapSaveCallback);
 
   ros::spin();
 
